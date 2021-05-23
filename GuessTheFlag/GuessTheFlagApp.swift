87ce2428651fb830d4da809eccabec1851f209be
alexandre.diasdasilva <alexandre.diasdasilva@yahoo.fr>
2021-05-23 18:40:29 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alexandre Dias Da Silva on 23/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
