efc6af9d2653d0d01b1c11d8402af7beeba2a8db
alexandre.diasdasilva <alexandre.diasdasilva@yahoo.fr>
2021-05-25 23:14:01 +0200

    Stacking up buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,10 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        ZStack {
+            
+            Color.blue.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                } .foregroundColor(.white)
+                
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(countries[number])
+                    }
+                }
+                
+                Spacer()
+                
+            }
+            
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                
+            }
+            )
+        }
+        
     }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
